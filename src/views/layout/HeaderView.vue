8d9e7e94b9643bb63346f8974d6f5363246c3786
xuliangzhan <xlz26296x@163.com>
2024-09-29 18:45:51 +0800

    update docs

--- src/views/layout/HeaderView.vue
+++ src/views/layout/HeaderView.vue
@@ -5,6 +5,10 @@
     </div>
     <div v-if="userInfo" class="header-right">
       <span class="right-item">
+        <vxe-link status="primary" href="/admin-template-v3/" target="_blank">切换 v3.x 版本</vxe-link>
+      </span>
+
+      <span class="right-item">
         <vxe-link icon="vxe-icon-github-fill" href="https://github.com/x-extends/vxe-admin-template" target="_blank">Github</vxe-link>
         <vxe-link href="https://gitee.com/x-extends/vxe-admin-template" target="_blank">
           <vxe-icon status="error" name="gitee-fill"></vxe-icon>
