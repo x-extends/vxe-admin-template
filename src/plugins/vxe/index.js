a041f99c7351372bb02e9eb4707cc9aef2f72d56
xuliangzhan <xlz26296x@163.com>
2024-11-13 16:29:09 +0800

    update docs

--- src/plugins/vxe/index.js
+++ src/plugins/vxe/index.js
@@ -1,6 +1,16 @@
-import './config'
+import VxeUI from 'vxe-pc-ui'
 
+import VxeUIPluginMenu from '@vxe-ui/plugin-menu'
+import VxeUIPluginExportXLSX from '@vxe-ui/plugin-export-xlsx'
+import VxeUIPluginRenderWangEditor from '@vxe-ui/plugin-render-wangeditor'
+import '@vxe-ui/plugin-render-wangeditor/dist/style.css'
+
+import './config'
 // 格式化
 import './format'
 // 渲染器
 import './render'
+
+VxeUI.use(VxeUIPluginMenu)
+VxeUI.use(VxeUIPluginExportXLSX)
+VxeUI.use(VxeUIPluginRenderWangEditor)
