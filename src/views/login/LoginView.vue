a62ee1050f0fbeedb589ff562e2ffdbdc070ecb6
xuliangzhan <xu_liangzhan@163.com>
2026-07-13 20:14:42 +0800

    update docs

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -2,10 +2,10 @@
   <div>
     <div class="login-title">
       <img src="@/assets/logo.png">
-      <span>Vxe 管理系统</span>
+      <span>用户登录</span>
     </div>
     <div class="login-desc">一个简洁高效、极致流畅的管理系统模板</div>
-    <vxe-form v-bind="formOptions" @submit="submitEvent">
+    <vxe-form class="login-form" v-bind="formOptions" @submit="submitEvent">
       <template #pwdAction>
         <vxe-row>
           <vxe-col span="12">
@@ -17,7 +17,7 @@
         </vxe-row>
       </template>
       <template #submitAction>
-        <vxe-button type="submit" status="primary" style="width: 100%;">立即登录</vxe-button>
+        <vxe-button type="submit" status="primary" size="xlarge" suffix-icon="vxe-icon-arrows-right" style="width: 100%;">立即登录</vxe-button>
       </template>
       <template #otherAction>
         <vxe-row>
@@ -29,22 +29,31 @@
             </span>
           </vxe-col>
         </vxe-row>
+        <div class="login-privacy">登录即代表您同意我们的 <vxe-button status="primary" mode="text" @click="openServiceEvent">服务条款</vxe-button> 和 <vxe-button status="primary" mode="text" @click="openPrivacyEvent">隐私协议</vxe-button></div>
       </template>
     </vxe-form>
+
+    <UserService ref="userServiceRef"></UserService>
+    <UserPrivacy ref="userPrivacyRef"></UserPrivacy>
   </div>
 </template>
 
 <script>
 import { mapActions } from 'vuex'
 import { VxeUI } from 'vxe-pc-ui'
+import UserService from './UserService.vue'
+import UserPrivacy from './UserPrivacy.vue'
 
 export default {
+  components: {
+    UserService,
+    UserPrivacy
+  },
   data () {
     const formOptions = {
       titleWidth: 100,
-      titleColon: true,
       titleAlign: 'right',
-      loading: false,
+      vertical: true,
       data: {
         name: 'test1',
         password: 'test1'
@@ -58,8 +67,8 @@ export default {
         ]
       },
       items: [
-        { field: 'name', title: '', span: 24, itemRender: { name: 'VxeInput', props: { prefixIcon: 'vxe-icon-user-fill', placeholder: '请输入用户名' } } },
-        { field: 'password', title: '', span: 24, itemRender: { name: 'VxePasswordInput', props: { prefixIcon: 'vxe-icon-lock-fill' } } },
+        { field: 'name', title: '用户名', span: 24, itemRender: { name: 'VxeInput', props: { size: 'xlarge', prefixIcon: 'vxe-icon-user-fill', placeholder: '请输入用户名' } } },
+        { field: 'password', title: '密码', span: 24, itemRender: { name: 'VxePasswordInput', props: { size: 'xlarge', prefixIcon: 'vxe-icon-lock-fill' } } },
         { span: 24, slots: { default: 'pwdAction' } },
         { span: 24, slots: { default: 'submitAction' } },
         { span: 24, slots: { default: 'otherAction' } }
@@ -75,6 +84,18 @@ export default {
     ...mapActions([
       'loginServer'
     ]),
+    openServiceEvent () {
+      const $userService = this.$refs.userServiceRef
+      if ($userService) {
+        $userService.open()
+      }
+    },
+    openPrivacyEvent () {
+      const $userPrivacy = this.$refs.userPrivacyRef
+      if ($userPrivacy) {
+        $userPrivacy.open()
+      }
+    },
     forgetPawdEvent () {
       VxeUI.modal.message({
         content: '无权限操作',
@@ -82,12 +103,14 @@ export default {
       })
     },
     submitEvent () {
-      this.formOptions.loading = true
+      VxeUI.loading.open({
+        text: '登录验证中...'
+      })
       this.loginServer(this.formOptions.data).then(() => {
-        this.formOptions.loading = false
+        VxeUI.loading.close()
         this.$router.push('/')
       }).catch(() => {
-        this.formOptions.loading = false
+        VxeUI.loading.close()
       })
     }
   }
--- src/views/login/RegisterView.vue
+++ src/views/login/RegisterView.vue
@@ -4,13 +4,16 @@
       <img src="@/assets/logo.png">
       <span>用户注册</span>
     </div>
-    <vxe-form v-bind="formOptions" @submit="submitEvent">
+    <vxe-form class="login-form" v-bind="formOptions" @submit="submitEvent">
       <template #privacyAction>
-        <vxe-checkbox v-model="allowAgreement">我已阅读并同意</vxe-checkbox>
-        <vxe-button status="primary" mode="text" @click="openAgreementEvent">《用户协议》</vxe-button>
+        <div class="login-privacy">
+          <vxe-checkbox v-model="allowAgreement">我已阅读并同意</vxe-checkbox>
+          <vxe-button status="primary" mode="text" @click="openServiceEvent">服务条款</vxe-button> 和
+          <vxe-button status="primary" mode="text" @click="openPrivacyEvent">隐私协议</vxe-button>
+        </div>
       </template>
       <template #submitAction>
-        <vxe-button type="submit" status="primary" style="width: 100%;">点击注册</vxe-button>
+        <vxe-button type="submit" status="primary" size="xlarge" style="width: 100%;">点击注册</vxe-button>
       </template>
       <template #otherAction>
         <vxe-row>
@@ -25,25 +28,27 @@
       </template>
     </vxe-form>
 
-    <UserAgreement ref="agreementRef"></UserAgreement>
+    <UserService ref="userServiceRef"></UserService>
+    <UserPrivacy ref="userPrivacyRef"></UserPrivacy>
   </div>
 </template>
 
 <script>
+import { VxeUI } from 'vxe-pc-ui'
 import { postPubAdminLoginRegister } from '@/api/login'
-import UserAgreement from './UserAgreement.vue'
+import UserService from './UserService.vue'
+import UserPrivacy from './UserPrivacy.vue'
 
 export default {
   components: {
-    UserAgreement
+    UserService,
+    UserPrivacy
   },
   data () {
     const formOptions = {
       titleWidth: 100,
-      titleColon: true,
       titleAlign: 'right',
       vertical: true,
-      loading: false,
       data: {
         name: '',
         password: '',
@@ -64,10 +69,10 @@ export default {
         ]
       },
       items: [
-        { field: 'name', title: '用户名', span: 24, itemRender: { name: 'VxeInput', props: { placeholder: '用户名由 4-14 位字母/数字组成', prefixIcon: 'vxe-icon-user-fill' } } },
-        { field: 'password', title: '密码', span: 24, itemRender: { name: 'VxePasswordInput', props: { placeholder: '密码由 4-18 位字母/数字/特殊字符组成', autoComplete: 'new-password', prefixIcon: 'vxe-icon-lock-fill' } } },
-        { field: 'confirmPassword', title: '确认密码', span: 24, itemRender: { name: 'VxePasswordInput', props: { placeholder: '请再次输入密码', autoComplete: 'new-password', prefixIcon: 'vxe-icon-lock-fill' } } },
-        { field: 'email', title: '邮箱', span: 24, itemRender: { name: 'VxeInput', props: { placeholder: '请输入邮箱', prefixIcon: 'vxe-icon-envelope-fill' } } },
+        { field: 'name', title: '用户名', span: 24, itemRender: { name: 'VxeInput', props: { size: 'xlarge', placeholder: '用户名由 4-14 位字母/数字组成', prefixIcon: 'vxe-icon-user-fill' } } },
+        { field: 'password', title: '密码', span: 24, itemRender: { name: 'VxePasswordInput', props: { size: 'xlarge', placeholder: '密码由 4-18 位字母/数字/特殊字符组成', autoComplete: 'new-password', prefixIcon: 'vxe-icon-lock-fill' } } },
+        { field: 'confirmPassword', title: '确认密码', span: 24, itemRender: { name: 'VxePasswordInput', props: { size: 'xlarge', placeholder: '请再次输入密码', autoComplete: 'new-password', prefixIcon: 'vxe-icon-lock-fill' } } },
+        { field: 'email', title: '邮箱', span: 24, itemRender: { name: 'VxeInput', props: { size: 'xlarge', placeholder: '请输入邮箱', prefixIcon: 'vxe-icon-envelope-fill' } } },
         { span: 24, slots: { default: 'privacyAction' } },
         { span: 24, slots: { default: 'submitAction' } },
         { span: 24, slots: { default: 'otherAction' } }
@@ -82,18 +87,26 @@ export default {
     }
   },
   methods: {
-    openAgreementEvent () {
-      const $agreement = this.$refs.agreementRef
-      if ($agreement) {
-        $agreement.open()
+    openServiceEvent () {
+      const $userService = this.$refs.userServiceRef
+      if ($userService) {
+        $userService.open()
+      }
+    },
+    openPrivacyEvent () {
+      const $userPrivacy = this.$refs.userPrivacyRef
+      if ($userPrivacy) {
+        $userPrivacy.open()
       }
     },
     submitEvent () {
-      this.formOptions.loading = true
+      VxeUI.loading.open({
+        text: '正在注册，请稍等片刻...'
+      })
       postPubAdminLoginRegister(this.formOptions.data).then(() => {
-        this.formOptions.loading = false
+        VxeUI.loading.close()
       }).catch(() => {
-        this.formOptions.loading = false
+        VxeUI.loading.close()
       })
     }
   }
